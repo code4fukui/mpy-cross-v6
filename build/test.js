a2890071d1d4b2069748dba03664cc2227c295c8
Taisuke Fukuno <fukuno@jig.jp>
2023-03-19 19:49:46 +0900

    fix url

--- build/test.js
+++ build/test.js
@@ -16,7 +16,7 @@ wait(1000)
 `;
 
 //const wasmurl = "http://localhost:8880/mpy-cross-v6.wasm";
-const wasmurl = "https://code4fukui.github.io/mpy-corss-v6/build/mpy-cross-v6.wasm";
+const wasmurl = "https://code4fukui.github.io/mpy-cross-v6/build/mpy-cross-v6.wasm";
 const bin = await compile("main.py", prog, null, wasmurl);
 //const bin = await compile("main.py", prog);
 console.log(bin.mpy);
